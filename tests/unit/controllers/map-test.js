b5c5c9b809ddf5ca537ce376eeaa45cded34b649
pugovka <ms.n.yunak@gmail.com>
2016-10-26 09:21:56 +1000

    Set index test and modify controller test

--- tests/unit/controllers/map-test.js
+++ tests/unit/controllers/map-test.js
@@ -1,51 +1,53 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:map', 'Unit | Controller | map', {});
+moduleFor('controller:map', 'Unit | Controller | map');
 
 test('creates url', function(assert) {
-  let mapController = this.subject();
+  const mapController = this.subject();
   const coordinates = {
     lat: 43.15520306319495,
     lng: 131.91798090934756
   };
+  const zoom = 15;
   const objectId = 108310688;
-  let newUrl = mapController.createUrl(coordinates, objectId);
+  let newUrl = mapController.createUrl(coordinates, zoom, objectId);
   
   assert.equal(
-    newUrl, '/map/43.15520306319495/131.91798090934756/108310688', 'should create url correctly with id'
+    newUrl, '/map/43.15520306319495/131.91798090934756/15/108310688', 'should create url correctly with id'
   );
 
-  newUrl = mapController.createUrl(coordinates);
+  newUrl = mapController.createUrl(coordinates, zoom);
   assert.equal(
-    newUrl, '/map/43.15520306319495/131.91798090934756', 'should create url correctly'
+    newUrl, '/map/43.15520306319495/131.91798090934756/15', 'should create url correctly'
   );
 });
 
 test('sets url', function(assert) {
-  let mapController = this.subject();
+  const mapController = this.subject();
   const coordinates = {
     lat: 43.15459257561516,
     lng: 131.91977798938754
   };
+  const zoom = 13;
   const objectId = 382440687;
 
-  mapController.setUrl(coordinates, objectId);
+  mapController.setUrl(coordinates, zoom, objectId);
   assert.equal(
-    window.location.pathname, '/map/43.15459257561516/131.91977798938754/382440687', 'should set url correctly with id'
+    window.location.pathname, '/map/43.15459257561516/131.91977798938754/13/382440687', 'should set url correctly with id'
   );
 
-  mapController.setUrl(coordinates);
+  mapController.setUrl(coordinates, zoom);
   assert.equal(
-    window.location.pathname, '/map/43.15459257561516/131.91977798938754', 'should set url correctly'
+    window.location.pathname, '/map/43.15459257561516/131.91977798938754/13', 'should set url correctly'
   );
 });
 
 test('set/unset current object id', function(assert) {
-  let mapController = this.subject();
+  const mapController = this.subject();
 
   mapController.setCurrentObjectId(382440687);
-  assert.equal(mapController.get('currentObjectId'), 382440687, 'ids should be matched (number)');
+  assert.equal(mapController.get('currentObjectId'), 382440687, 'ids should be equal (number)');
 
-  mapController.setCurrentObjectId(undefined);
-  assert.equal(mapController.get('currentObjectId'), undefined, 'ids should be matched (undefined)');
+  mapController.setCurrentObjectId(null);
+  assert.equal(mapController.get('currentObjectId'), null, 'ids should be equal (null)');
 });
